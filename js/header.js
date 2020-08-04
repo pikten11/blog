a213fdf9893454dd580d001e9bb0cf01a6c1eff3
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-08-04 14:15:26 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,5 +1,13 @@
 function header(){
 var html = "";
-  html += '<div class="cp_offcm01">';
+	html += '<div class="header">';
+	html += '<div class="container">';
+	html += '<div class="header-right">';
+	html += '<img src="" alt="pikten">';
+	html += '<div class="header-left">';
+	html += '<a href="https://pikten11.github.io/blog/index.html">トップページ</a>';
+	html += '<a href="https://pikten11.github.io/blog/form.html">お問い合わせ</a>';
+	html += '</div>';
+	html += '</div>';
 	document.write(html);
 }
