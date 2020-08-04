80d1bac85619cf9f4e984b0d3ddb18f5b0cee071
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-08-04 21:46:09 +0900

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,16 @@
-function header(){
+function footer(){
   var html = "";
-  html += '<div class="footer">';
+	html += '<div class="footer">';
 	html += '<div class="container">';
 	html += '<div class="footer-right">';
-  html += '<h2>サイトをシェア</h2>
+	html += '<h2>サイトをシェア</h2>
 	html += '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="pikten(獣の集い):blog" data-url="https://pikten11.github.io/blog/index.html" data-show-count="false">Tweet</a>';
-  html += '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+	html += '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
 	html += '<div class="footer-bottom">';
 	html += '<p>©2020 pikten</p>';
 	html += '<div class"clear">';
 	html += '</div>';
 	html += '</div>';
 	html += '</div>';
-  document.write(html);
+	document.write(html);
 }
