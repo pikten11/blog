632b5b21bec491c1121b98691069b714e1071130
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-09-22 09:56:25 +0900

    Update and rename part1.html to part1.php

--- course/minecraft/addon/Level0/part1.php
+++ course/minecraft/addon/Level0/part1.php
@@ -1,3 +1,16 @@
+<?php
+// カウント数取得関数
+function get_count($file) {
+	$filename = 'data/'.$file.'.dat';
+	$fp = @fopen($filename, 'r');
+	if ($fp) {
+		$vote = fgets($fp, 9182);
+	} else {
+		$vote = 0;
+	}
+	return $vote;
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
